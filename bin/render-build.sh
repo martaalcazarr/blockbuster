01d76677772813f5a710bdbe36ce55ba50efe8a2
martaalcazarr <martaalcazarredondo@gmail.com>
2023-07-09 23:45:42 -0400

    add render build

---
+++ bin/render-build.sh
@@ -0,0 +1,9 @@
+#!usr/bin/env bash
+# exit on error
+set -0 errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exect rake assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seed
